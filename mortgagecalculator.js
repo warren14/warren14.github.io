9ce3e58ff84c0b7a716530d421967e41739e2a76
warren14 <63477673+warren14@users.noreply.github.com>
2021-02-04 11:57:34 -0800

    Add files via upload

--- mortgagecalculator.js
+++ mortgagecalculator.js
@@ -57,7 +57,7 @@ document.querySelector('#calculate').addEventListener('click',
 function() {
 let loanAmount = Number(document.querySelector('#loanAmount').value);
 let  years = Number(document.querySelector('#years').value);
-let interest = Number(document.querySelector('#interest').value);
+let interest = 1 + (Number(document.querySelector('#interest').value)/100);
 let payment = 0; 
 console.log(loanAmount, years, interest, payment, typeof (loanAmount, years, interest, payment));
 
